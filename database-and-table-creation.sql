d99c8b619c9a139ad32a8b9424dfb861ffe4d382
edreeseg <edward.reeseg@gmail.com>
2019-05-14 09:03:50 -0400

    Adding feedback table, phone_number to users table, changing attributes without a specific length to TEXT, rather than VARCHAR (only state_abbr and zip_code remain VARCHAR).

--- database-and-table-creation.sql
+++ database-and-table-creation.sql
@@ -3,64 +3,79 @@ CREATE DATABASE contractor_scheduler_test;
 
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-CREATE TABLE contractors (
-    id UUID NOT NULL DEFAULT uuid_generate_v4() PRIMARY KEY,
-    name VARCHAR(50) NOT NULL,
-    phone_number VARCHAR(15) NOT NULL UNIQUE,
-    street_address VARCHAR(50) NOT NULL,
-    city VARCHAR(25) NOT NULL,
-    state_abbr VARCHAR(2) NOT NULL,
-    zip_code VARCHAR(10) NOT NULL,
-    created_at TIMESTAMP DEFAULT now()
+CREATE TABLE IF NOT EXISTS contractors (
+  id UUID NOT NULL DEFAULT uuid_generate_v4() PRIMARY KEY,
+  name TEXT NOT NULL,
+  phone_number TEXT NOT NULL UNIQUE,
+  street_address TEXT NOT NULL,
+  city TEXT NOT NULL,
+  state_abbr VARCHAR(2) NOT NULL,
+  zip_code VARCHAR(10) NOT NULL,
+  created_at TIMESTAMP DEFAULT NOW()
 );
-
-CREATE TABLE schedules (
-    contractor_id UUID NOT NULL,
-    start_time TIMESTAMPTZ NOT NULL,
-    duration INTERVAL NOT NULL,
-    UNIQUE (contractor_id, start_time),
-    created_at TIMESTAMP DEFAULT now(),
-    FOREIGN KEY (contractor_id) REFERENCES contractors(id)
-        ON DELETE CASCADE
+    
+CREATE TABLE IF NOT EXISTS schedules (
+  contractor_id UUID NOT NULL,
+  start_time TIMESTAMPTZ NOT NULL,
+  duration INTERVAL NOT NULL,
+  created_at TIMESTAMP DEFAULT NOW(),
+  UNIQUE (contractor_id, start_time),
+  PRIMARY KEY (contractor_id, start_time),
+  FOREIGN KEY (contractor_id) REFERENCES contractors(id)
+  ON DELETE CASCADE
 );
-
-CREATE TABLE users (
-    id UUID NOT NULL DEFAULT uuid_generate_v4() PRIMARY KEY,
-    username VARCHAR(50) NOT NULL UNIQUE,
-    password VARCHAR(50) NOT NULL UNIQUE,
-    email VARCHAR(50) NOT NULL UNIQUE,
-    contractor_id UUID DEFAULT NULL,
-    created_at TIMESTAMP DEFAULT now(),
-    FOREIGN KEY (contractor_id) REFERENCES contractors(id)
-        ON DELETE CASCADE
+    
+CREATE TABLE IF NOT EXISTS users (
+  id UUID NOT NULL DEFAULT uuid_generate_v4() PRIMARY KEY,
+  google_id TEXT NOT NULL UNIQUE,
+  username TEXT NOT NULL UNIQUE,
+  phone_number TEXT NOT NULL UNIQUE,
+  email TEXT NOT NULL UNIQUE,
+  contractor_id UUID DEFAULT NULL,
+  created_at TIMESTAMP DEFAULT NOW(),
+  FOREIGN KEY (contractor_id) REFERENCES contractors(id)
+  ON DELETE CASCADE
 );
-
-CREATE TABLE services (
-    id UUID NOT NULL DEFAULT uuid_generate_v4() PRIMARY KEY,
-    name VARCHAR(50) NOT NULL,
-    price MONEY DEFAULT NULL,
-    contractor_id UUID NOT NULL,
-    created_at TIMESTAMP DEFAULT now(),
-    FOREIGN KEY (contractor_id) REFERENCES contractors(id)
-        ON DELETE CASCADE
+ 
+CREATE TABLE IF NOT EXISTS services (
+  id UUID NOT NULL DEFAULT uuid_generate_v4() PRIMARY KEY,
+  name TEXT NOT NULL,
+  price MONEY DEFAULT NULL,
+  contractor_id UUID NOT NULL,
+  created_at TIMESTAMP DEFAULT NOW(),
+  FOREIGN KEY (contractor_id) REFERENCES contractors(id)
+  ON DELETE CASCADE
 );
-
-CREATE TABLE appointments (
-    id UUID NOT NULL UNIQUE DEFAULT uuid_generate_v4() PRIMARY KEY,
-    contractor_id UUID NOT NULL,
-    user_id UUID NOT NULL,
-    service_id UUID NOT NULL,
-    appointment_datetime TIMESTAMPTZ NOT NULL,
-    duration INTERVAL NOT NULL,
-    created_at TIMESTAMP DEFAULT now(),
-    UNIQUE(contractor_id, user_id, service_id, appointment_datetime),
-    FOREIGN KEY (contractor_id) REFERENCES contractors(id)
-        ON DELETE CASCADE,
-    FOREIGN KEY (user_id) REFERENCES users(id)
-        ON DELETE CASCADE,
-    FOREIGN KEY (service_id) REFERENCES services(id)
-        ON DELETE SET NULL -- Do not delete scheduled appointment if a service is removed - potential for abuse
+    
+CREATE TABLE IF NOT EXISTS appointments (
+  id UUID NOT NULL UNIQUE DEFAULT uuid_generate_v4() PRIMARY KEY,
+  contractor_id UUID NOT NULL,
+  user_id UUID NOT NULL,
+  service_id UUID NOT NULL,
+  appointment_datetime TIMESTAMPTZ NOT NULL,
+  duration INTERVAL NOT NULL,
+  created_at TIMESTAMP DEFAULT NOW(),
+  UNIQUE(contractor_id, user_id, service_id, appointment_datetime),
+  FOREIGN KEY (contractor_id) REFERENCES contractors(id)
+  ON DELETE CASCADE,
+  FOREIGN KEY (user_id) REFERENCES users(id)
+  ON DELETE CASCADE,
+  FOREIGN KEY (service_id) REFERENCES services(id)
+  ON DELETE SET NULL
 );
+    
+CREATE TABLE feedback (
+  id UUID NOT NULL UNIQUE DEFAULT uuid_generate_v4() PRIMARY KEY,
+  user_id UUID NOT NULL,
+  contractor_id UUID NOT NULL,
+  stars INT NOT NULL,
+  message TEXT NOT NULL,
+  created_at TIMESTAMP DEFAULT NOW(),
+  FOREIGN KEY (user_id) REFERENCES users(id)
+  ON DELETE SET NULL,
+  FOREIGN KEY (contractor_id) REFERENCES contractors(id)
+  ON DELETE SET NULL
+); 
 
 SELECT * FROM schedules -- Query to check if appointment falls within availability 
 WHERE contractor_id = ${contractor_id}
@@ -71,4 +86,4 @@ WHERE contractor_id = ${contractor_id}
 AND NOT (start_time <= (${start_time} + ${duration}) AND (start_time + duration) >= ${start_time});
 
 DELETE FROM appointments -- Run on a schedule, cron job?
-    WHERE appointment_datetime < (now() - INTERVAL '30 DAYS')
+    WHERE appointment_datetime < (NOW() - INTERVAL '30 DAYS')
